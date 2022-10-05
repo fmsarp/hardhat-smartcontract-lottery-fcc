5a48fd8204416c863bc04d0432ddb2049bd10c35
Franklin Sarpong <fmsarp@gmail.com>
2022-10-05 07:34:01 -0400

    third commit

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -12,21 +12,28 @@ pragma solidity ^0.8.8;
 error Raffle__NotEnoughETHEntered();
 
 contract Raffle {
+    /* State Variables */
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() {
+    function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
         }
+        s_players.push(payable(msg.sender));
     }
 
     // function pickRandomWinner() {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 }
