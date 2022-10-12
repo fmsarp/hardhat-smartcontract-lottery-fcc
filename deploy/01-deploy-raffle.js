93a93dd15f1f7ad5e3dbde84684e3f2a9343f9d8
Franklin Sarpong <fmsarp@gmail.com>
2022-10-12 00:12:54 -0400

    commit 13th

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,5 +1,6 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
 
@@ -16,18 +17,38 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
         const transactionReceipt = await transactionResponse.wait(1)
         subscriptionId = transactionReceipt.events[0].args.subId
         // Funding subscription on real network require LINK
-        await vrfCoordinatorV2Mock.funcSubsciption(subscriptionId, VRF_SUB_FUND_AMOUNT)
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"]
     const gasLane = networkConfig[chainId]["gasLane"]
-    const args = [vrfCoordinatorV2Address, entranceFee, gasLane]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        gasLane,
+        subscriptionId,
+        callbackGasLimit,
+        interval,
+    ]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+
+    log("---------------------------------------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
